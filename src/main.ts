198c6cbc7c729c7ebd933b691e72a2763012af6b
watatakahashi <takawata0623@yahoo.co.jp>
2019-08-12 15:56:10 +0900

    add main.ts

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import * as firebase from 'firebase/app'
+import 'firebase/firestore'
+
+Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_apiKey,
+  authDomain: process.env.VUE_APP_authDomain,
+  databaseURL: process.env.VUE_APP_databaseURL,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId: process.env.VUE_APP_messagingSenderId,
+  appId: process.env.VUE_APP_appId
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
